374c275bf56d52c0a30ad28283f7258697cef55b
Raphaon <raphaondobo@gmail.com>
2025-10-27 17:25:38 -0400

    Updating lavavel projet

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -19,5 +19,9 @@ class TrustProxies extends Middleware
      *
      * @var int
      */
-    protected $headers = Request::HEADER_X_FORWARDED_ALL;
+    protected $headers = \Symfony\Component\HttpFoundation\Request::HEADER_X_FORWARDED_FOR
+                   | \Symfony\Component\HttpFoundation\Request::HEADER_X_FORWARDED_HOST
+                   | \Symfony\Component\HttpFoundation\Request::HEADER_X_FORWARDED_PORT
+                   | \Symfony\Component\HttpFoundation\Request::HEADER_X_FORWARDED_PROTO;
+
 }
